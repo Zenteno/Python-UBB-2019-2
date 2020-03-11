15cc5270a459f0e6a6a8bce0ada5f89b42c8a079
Tu Nombre <you@example.com>
2020-03-11 11:16:00 -0300

    first commit

---
+++ 11_03_2020/farmacia.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+	url = "https://farmanet.minsal.cl/index.php/ws/getLocales"
+	r  =  requests.get(url)
+	diccionario = {}
+	for objeto in r.json():
+		llave_region = f"region_{objeto['fk_region']}" 
+		if llave_region not in diccionario:
+			diccionario[llave_region] = []
+		if objeto["comuna_nombre"] not in diccionario[llave_region]:
+			diccionario[llave_region].append(objeto["comuna_nombre"])
+
+	return jsonify(diccionario)
+
+if __name__ == "__main__":
+	app.run(debug=True)
